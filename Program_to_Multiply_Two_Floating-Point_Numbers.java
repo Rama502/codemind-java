dc01e4181c2af99a5f5728ad29d55fe316d7b9d5
Rama502 <97427355+Rama502@users.noreply.github.com>
2023-02-03 20:01:40 +0530

    import java.util.*; class multiply{     public static void main(String args[])     {         float a,b,multiply;         Scanner sc=new Scanner(System.in);         a=sc.nextFloat();         b=sc.nextFloat();         multiply=(a*b);         System.out.format("%.2f",multiply);     } }

---
+++ Program_to_Multiply_Two_Floating-Point_Numbers.java
@@ -0,0 +1,12 @@
+import java.util.*;
+class multiply{
+    public static void main(String args[])
+    {
+        float a,b,multiply;
+        Scanner sc=new Scanner(System.in);
+        a=sc.nextFloat();
+        b=sc.nextFloat();
+        multiply=(a*b);
+        System.out.format("%.2f",multiply);
+    }
+}
