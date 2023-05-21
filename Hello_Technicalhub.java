3d43e27bf2f6c61de52c186013a61b866cc67a8a
Rama502 <97427355+Rama502@users.noreply.github.com>
2023-05-21 11:47:26 +0530

    import java.util.*; public class hello {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         String s1;         s1=sc.nextLine();         System.out.println("Hello Technicalhub");         System.out.println(s1);     } }

---
+++ Hello_Technicalhub.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class hello
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        String s1;
+        s1=sc.nextLine();
+        System.out.println("Hello Technicalhub");
+        System.out.println(s1);
+    }
+}
